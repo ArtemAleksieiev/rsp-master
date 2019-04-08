eb247b295cdc94d409b799b07135c6602f228af8
Artem Aleksieiev <artemaleksieiev@Artems-iMac.home>
2019-04-08 11:55:24 -0400

    Updates

---
+++ precache-manifest.1d8c3b7fd3ff17e3044d50af9cb87667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dbb7552c7b7176d0e56e",
+    "url": "/rsp-master/static/js/runtime~main.9b1b0f44.js"
+  },
+  {
+    "revision": "2219049702f6b6f4f2ea",
+    "url": "/rsp-master/static/js/main.a52efc62.chunk.js"
+  },
+  {
+    "revision": "8d2285c872170492082f",
+    "url": "/rsp-master/static/js/2.8994b6f6.chunk.js"
+  },
+  {
+    "revision": "2219049702f6b6f4f2ea",
+    "url": "/rsp-master/static/css/main.c555d2d9.chunk.css"
+  },
+  {
+    "revision": "749e9f270b44562eec955104dc6a6385",
+    "url": "/rsp-master/index.html"
+  }
+];
